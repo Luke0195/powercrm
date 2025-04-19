c4e60d23a04218fa30b4a79150486a5e6276832a
Lucas Santos <lucas201273@gmail.com>
2025-04-19 15:24:04 -0300

    test: ensure VehicleControllerImpl returns 400 if no user_id is provided

--- src/test/java/br/com/powercrm/app/controller/impl/VehicleControllerImplTest.java
+++ src/test/java/br/com/powercrm/app/controller/impl/VehicleControllerImplTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import javax.xml.transform.Result;
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -24,12 +25,15 @@ import java.util.UUID;
 @ActiveProfiles("dev")
 class VehicleControllerImplTest {
 
+    private static final String ROUTE_NAME = "/vehicle";
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
     private VehicleRequestDto vehicleRequestDto;
 
+    private UUID existingId = UUID.randomUUID();
     @BeforeEach
     void setup(){
         this.vehicleRequestDto = VehicleFactory.makeVehicleRequestDto();
@@ -38,9 +42,49 @@ class VehicleControllerImplTest {
     @DisplayName("POST - handleAddVehicle should returns 400 if no plate is provided")
     @Test
     void handleAddVehicleShouldReturnsBadRequestWhenNoPlateIsProvided() throws Exception{
-        VehicleRequestDto vehicleRequestDto= new VehicleRequestDto(null, BigDecimal.valueOf(30.000), 2010, UUID.randomUUID());
+        VehicleRequestDto vehicleRequestDto= new VehicleRequestDto(null, BigDecimal.valueOf(30.000), 2010,
+                existingId);
         String jsonBody = objectMapper.writeValueAsString(vehicleRequestDto);
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/vehicle")
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(ROUTE_NAME)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(jsonBody));
+        resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @DisplayName("POST - handleAddVehicle should returns 400 if advertised_plate is provided")
+    @Test
+    void handleAddVehicleShouldReturnsBadRequestWhenNoAdvertisedPlateIsProvided() throws Exception{
+        VehicleRequestDto vehicleRequestDto = new VehicleRequestDto("any_plate", null, 2010,
+                existingId);
+        String jsonBody = objectMapper.writeValueAsString(vehicleRequestDto);
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(ROUTE_NAME)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(jsonBody));
+        resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @DisplayName("POST - handleAddVehicle should returns 400 if no year is provided")
+    @Test
+    void handleAddVehicleShouldReturnsBadRequestWheNoYearIsProvided() throws Exception{
+        VehicleRequestDto vehicleRequestDto = new VehicleRequestDto("any_plate", BigDecimal.valueOf(30.000),
+                null, existingId);
+        String jsonBody = objectMapper.writeValueAsString(vehicleRequestDto);
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(ROUTE_NAME)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(jsonBody));
+        resultActions.andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @DisplayName("POST - handleAddVehicle should returns 400 if no user_id is provided")
+    @Test
+    void handleAddVehicleShouldReturnsBadRequestWheNoUserIdIsProvided() throws Exception{
+        VehicleRequestDto vehicleRequestDto = new VehicleRequestDto("any_plate", BigDecimal.valueOf(30.000),
+                2015, null);
+        String jsonBody = objectMapper.writeValueAsString(vehicleRequestDto);
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(ROUTE_NAME)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(jsonBody));
