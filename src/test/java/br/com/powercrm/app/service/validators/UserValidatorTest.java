0213d4e2db698d123a3fddebbeb0ffe93b577db2
Lucas Santos <lucas201273@gmail.com>
2025-04-19 11:13:33 -0300

    test: ensure UserValidatorTest should calls mapData on succes

--- src/test/java/br/com/powercrm/app/service/validators/UserValidatorTest.java
+++ src/test/java/br/com/powercrm/app/service/validators/UserValidatorTest.java
@@ -1,5 +1,6 @@
 package br.com.powercrm.app.service.validators;
 
+import br.com.powercrm.app.domain.entities.User;
 import br.com.powercrm.app.dto.request.UserRequestDto;
 import br.com.powercrm.app.factories.UserFactory;
 import br.com.powercrm.app.repository.UserRepository;
@@ -36,5 +37,33 @@ class UserValidatorTest {
         Assertions.assertThrows(ResourceAlreadyExistsException.class, () ->{
             userValidator.validateUniqueFields(userRequestDto);
         });
+    }
+
+    @DisplayName("ValidateUniqueFields should throws ResourceAlreadyExistsException when existing cpf is provided")
+    @Test
+    void validateUniqueFieldsShouldReturnsResourceAlreadyExistsWhenExistingCpfIsProvided(){
+        Mockito.when(userRepository.existsByCpf(Mockito.any())).thenThrow(ResourceAlreadyExistsException.class);
+        Assertions.assertThrows(ResourceAlreadyExistsException.class, () -> {
+            userValidator.validateUniqueFields(userRequestDto);
+        });
+    }
+
+    @DisplayName("ValidateUniqueFields should returns nothing on success")
+    @Test
+    void validateUniqueFieldsShouldReturnsNothingOnSuccess(){
+        userValidator.validateUniqueFields(userRequestDto);
+        Mockito.verify(userRepository).existsByEmail(Mockito.any());
+        Mockito.verify(userRepository).existsByCpf(Mockito.any());
+    }
+
+    @DisplayName("MapDate should map data from UserRequestDo to User entity")
+    @Test
+    void mapDataShouldReturnsAnUserResponseDtoOnSuccess(){
+        UserRequestDto userRequestDto = UserFactory.makeUserRequestDto();
+        User user = new User();
+        userValidator.mapData(userRequestDto, user);
+        Assertions.assertEquals("any_name", user.getName());
+        Assertions.assertEquals("any_mail@mail.com", user.getEmail());
+        Assertions.assertEquals("640.290.140-70", user.getCpf());
     }
 }
