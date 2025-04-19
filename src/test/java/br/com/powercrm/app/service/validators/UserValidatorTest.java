98153d54416832cdc1570d039fbb3c96e1db96e7
Lucas Santos <lucas201273@gmail.com>
2025-04-19 10:57:06 -0300

    test: ensure UserValidator throws ResourceAlreadyExists when email already exists

--- src/test/java/br/com/powercrm/app/service/validators/UserValidatorTest.java
+++ src/test/java/br/com/powercrm/app/service/validators/UserValidatorTest.java
@@ -1,13 +1,19 @@
 package br.com.powercrm.app.service.validators;
 
+import br.com.powercrm.app.dto.request.UserRequestDto;
+import br.com.powercrm.app.factories.UserFactory;
 import br.com.powercrm.app.repository.UserRepository;
+import br.com.powercrm.app.service.exceptions.ResourceAlreadyExistsException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserValidatorTest {
@@ -17,8 +23,18 @@ class UserValidatorTest {
     @Mock
     private UserRepository userRepository;
 
-    @DisplayName("ValidateUniqueFields should throws ResourceAlreadyExistsException when existing email is provided")
-    void validateUniqueFieldsShouldReturnResourceAlreadyExistsWhenAnExistingEmailIsProvided(){
+    private UserRequestDto userRequestDto;
 
+    @BeforeEach
+    void setup(){
+        this.userRequestDto = UserFactory.makeUserRequestDto();
+    }
+    @DisplayName("ValidateUniqueFields should throws ResourceAlreadyExistsException when existing email is provided")
+    @Test
+    void validateUniqueFieldsShouldReturnResourceAlreadyExistsWhenAnExistingEmailIsProvided(){
+        Mockito.when(userRepository.existsByEmail(Mockito.any())).thenThrow(ResourceAlreadyExistsException.class);
+        Assertions.assertThrows(ResourceAlreadyExistsException.class, () ->{
+            userValidator.validateUniqueFields(userRequestDto);
+        });
     }
 }
