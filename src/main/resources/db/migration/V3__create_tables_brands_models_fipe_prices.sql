7931963944297208f12bec2c094d7eccf7034b6a
Lucas Santos <lucas201273@gmail.com>
2025-04-23 22:42:21 -0300

    feat: add postgres to docker compose up

--- src/main/resources/db/migration/V3__create_tables_brands_models_fipe_prices.sql
+++ src/main/resources/db/migration/V3__create_tables_brands_models_fipe_prices.sql
@@ -1,18 +1,18 @@
 CREATE TABLE tb_brands(
-  id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(45) NOT NULL,
   brand_code VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE tb_models(
-    id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id SERIAL  NOT NULL PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
     model_code VARCHAR(45) NOT NULL
 );
 
 
-ALTER TABLE tb_vehicles ADD COLUMN  brand_id SMALLINT;
-ALTER TABLE tb_vehicles ADD COLUMN  model_id SMALLINT;
+ALTER TABLE tb_vehicles ADD COLUMN  brand_id SERIAL;
+ALTER TABLE tb_vehicles ADD COLUMN  model_id SERIAL;
 ALTER TABLE tb_vehicles ADD COLUMN fipe_price NUMERIC;
 ALTER TABLE tb_vehicles ADD CONSTRAINT fk_brand_id FOREIGN KEY(brand_id) REFERENCES tb_brands(id);
 ALTER TABLE tb_vehicles ADD CONSTRAINT fk_model_id FOREIGN KEY (model_id) REFERENCES tb_models(id);
